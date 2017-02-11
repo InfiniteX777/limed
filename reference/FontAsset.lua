8f4326570f8cc1a17187182f3f181993c4e867b4
InfiniteX777 <InfiniteX777@gmail.com>
2017-02-11 23:58:33 +0800

    Efficiency.

--- reference/FontAsset.lua
+++ reference/FontAsset.lua
@@ -1,40 +1,13 @@
-local hori = {
-	center = function(pos,screen)
-		return Vector2:new(pos.x-screen/2,pos.y)
-	end,
-	right = function(pos,screen)
-		return Vector2:new(pos.x-screen,pos.y)
-	end
-}
-
 local alignment = {
-	topleft = function(pos)
-		return pos,0,"left"
-	end,
-	middleleft = function(pos)
-		return pos,-0.5,"left"
-	end,
-	bottomleft = function(pos)
-		return pos,-1,"left"
-	end,
-	topcenter = function(...)
-		return hori.center(...),0,"center"
-	end,
-	middlecenter = function(...)
-		return hori.center(...),-0.5,"center"
-	end,
-	bottomcenter = function(...)
-		return hori.center(...),-1,"center"
-	end,
-	topright = function(...)
-		return hori.right(...),0,"right"
-	end,
-	middleright = function(...)
-		return hori.right(...),-0.5,"right"
-	end,
-	bottomright = function(...)
-		return hori.right(...),-1,"right"
-	end
+	topleft = {0,0,"left"},
+	middleleft = {0,-0.5,"left"},
+	bottomleft = {0,-1,"left"},
+	topcenter = {0.5,0,"center"},
+	middlecenter = {0.5,-0.5,"center"},
+	bottomcenter = {0.5,-1,"center"},
+	topright = {1,0,"right"},
+	middleright = {1,-0.5,"right"},
+	bottomright = {1,-1,"right"}
 }
 
 local FontAsset = Instance:class("FontAsset",4)({
@@ -44,22 +17,22 @@ local FontAsset = Instance:class("FontAsset",4)({
 	color = Color:new(),
 	wrap = nil,
 	draw = function(self,x,y,angle,sx,sy)
-		local screen = love.graphics.getDimensions()/(sx^2)
 		local font = self.font
+		local width = love.graphics.getDimensions()
 		local height = font:getHeight()*sy
-		local pos,direction,align = alignment[self.align](
-			Vector2:new(x,y):rotateToVectorSpace(Vector2:new(),-angle),
-			screen
-		)
+		local pos = Vector2:new(x,y):rotateToVectorSpace(Vector2:new(),-angle)
+		local offset,direction,align = unpack(alignment[self.align])
+		offset = offset*sx
+		love.graphics.push()
 		love.graphics.setColor(self.color:components())
 		love.graphics.rotate(angle)
 
 		if self.wrap then
 			local _,list = font:getWrap(self.text,self.wrap)
 			direction = #list*direction
-			love.graphics.printf(table.concat(list,"\n"),pos.x,pos.y+direction*height,screen,align,0,sx,sy)
-		else love.graphics.printf(self.text,pos.x,pos.y+height*direction,screen,align,0,sx,sy)
+			love.graphics.printf(table.concat(list,"\n"),pos.x-width*offset,pos.y+height*direction,width,align,0,sx,sy)
+		else love.graphics.printf(self.text,pos.x-width*offset,pos.y+height*direction,width,align,0,sx,sy)
 		end
-		love.graphics.origin()
+		love.graphics.pop()
 	end
 })
